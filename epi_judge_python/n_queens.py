e3f3b8516ec8e4096b80919f02ea6ce9763e0b45
Brandon Morris <brandon.morris@experian.com>
2019-03-21 16:03:31 -0700

    Solve n queens

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -2,8 +2,33 @@ from test_framework import generic_test
 
 
 def n_queens(n):
-    # TODO - you fill in here.
-    return []
+    boards = []
+
+    def queens_inner(row, queens):
+        if row == n:
+            boards.append(queens.copy())
+        else:
+            for col in range(n):
+                queens.append(col)
+                if is_valid(queens):
+                    queens_inner(row + 1, queens)
+                queens.pop(-1)
+
+    queens_inner(0, [])
+    return boards
+
+
+
+
+def is_valid(queens):
+    positions = [(row, col) for row, col in enumerate(queens)]
+    for i in range(len(queens) - 1):
+        for j in range(i + 1, len(queens)):
+            q1, q2 = positions[i], positions[j]
+            if (q1[0] == q2[0] or q1[1] == q2[1] or
+                    abs(q1[0] - q2[0]) == abs(q1[1] - q2[1])):
+                return False
+    return True
 
 
 def comp(a, b):
