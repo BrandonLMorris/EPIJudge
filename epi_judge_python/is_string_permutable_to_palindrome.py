67c35e1f8bc88d7e95b028195e6f95cc0363d1e9
Brandon Morris <brandon.morris@experian.com>
2019-03-25 15:51:24 -0700

    Solve a couple more problems

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -2,7 +2,20 @@ from test_framework import generic_test
 
 
 def can_form_palindrome(s):
-    # TODO - you fill in here.
+    # Convert string into counts of each character
+    counts = {}
+    for c in s:
+        if c not in counts:
+            counts[c] = 0
+        counts[c] += 1
+    # Check if every count (except at most one) is even
+    found_odd = False
+    for c, count in counts.items():
+        if count % 2 != 0:
+            if found_odd:
+                return False
+            else:
+                found_odd = True
     return True
 
 
--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -2,8 +2,18 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A):
-    # TODO - you fill in here.
-    return []
+
+    def perms_inner(prefix, possible):
+        if len(possible) == 0:
+            return prefix
+        return [(prefix + [possible[i]], possible[0:i] + possible[i+1:]) for i in range(len(possible))]
+    possible = perms_inner([], A)
+    for _ in range(len(A) - 1):
+        tmp = []
+        for t in possible:
+            tmp.extend(perms_inner(*t))
+        possible = tmp
+    return [p[0] for p in possible]
 
 
 if __name__ == '__main__':
